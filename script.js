785186cff829b53d40358baadb916020d9517942
joongki-J <surprise861218@gmail.com>
2026-09-10 19:54:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -21,12 +21,12 @@ const TOOLS = [
     url: "./gradient-descent-2/"
   },
   {
-    title: "정적분과 리만합 탐구",
+    title: "시어핀스키삼각형",
     subject: "미적분Ⅱ",
     symbol: "∫",
     description: "직사각형 넓이의 합이 정적분 값에 가까워지는 과정을 단계별로 관찰합니다.",
     tags: ["정적분", "리만합", "넓이"],
-    url: "./riemann-sum/"
+    url: "./sierpinski/"
   },
   {
     title: "몬테카를로 원주율 실험",
